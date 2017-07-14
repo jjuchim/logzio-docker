16126b59e5a0cd96903d3d58f6fe8d7285e3bf72
Jonas Juchim <jjuchim@googlemail.com>
2017-07-14 14:43:47 +0200

    Change logging logic

--- index.js
+++ index.js
@@ -26,65 +26,30 @@ function start() {
       }),
       new (winston.transports.Logstash)({
         port: process.env.LOGSTASH_PORT,
-        host: process.env.LOGSTASH_HOST
+        host: process.env.LOGSTASH_HOST,
+        formatter: function(options) { return options.message; }
       })
     ]
   })
 
-  var logFunctionGenerator = function (parseFunction) {
-    return through.obj(function (log, _, callback) {
-      if(parseFunction) { log = parseFunction(log); }
-
-      logger.log('info', JSON.stringify(log))
-      callback()
-    })
-  };
+  var log = through.obj(function (log, _, callback) {
+    logger.log('info', log)
+    callback()
+  });
 
   // Docker Logs
-  var parseLog = function (log) {
-   if(isJSON(log.line)){
-      log.line = JSON.parse(log.line)
-      log = parseMessage(log)
-    }else{
-      log.message = log.line;
-      delete log.line;
-    }
-    return log;
-  };
-
   loghose = logFactory({events: events});
-  loghose.pipe(logFunctionGenerator(parseLog));
+  loghose.pipe(log);
 
   // Docker Stats
   dockerStatsSource = statsFactory({events: events, statsinterval: 30});
-  dockerStatsSource.pipe(logFunctionGenerator());
+  dockerStatsSource.pipe(log);
 
   // Docker Events
   dockerEventSource = eventsFactory({});
-  dockerEventSource.pipe(logFunctionGenerator());
+  dockerEventSource.pipe(log);
 
   return loghose;
-};
-
-function parseMessage(msg) {
-  var renameField = function(obj, oldName, newName) {
-    obj[newName] = obj[oldName];
-    delete obj[oldName];
-    return obj
-  };
-
-  var log = renameField(msg, 'line', 'message')
-  log.message = renameField(log.message, '_request.body', 'request_body')
-  log.message = renameField(log.message, '_request.status', 'status')
-  log.message = renameField(log.message, '_request.response-body', 'response_body')
-  log.message = renameField(log.message, '_request.method', 'method')
-  log.message = renameField(log.message, '_request.path', 'path')
-  log.message = renameField(log.message, '_request.params', 'params')
-  log.message = renameField(log.message, '_request.request_id', 'request_id')
-  log.message = renameField(log.message, '_request.request_ip', 'request_ip')
-  log.message = renameField(log.message, '_request.user_agent', 'user_agent')
-  log.message = renameField(log.message, '_request.duration', 'duration')
-  return log;
 };
 
 module.exports = start;
