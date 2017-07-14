b85ee3d639d022a769b4f204b59d055892243b62
Jonas Juchim <jjuchim@googlemail.com>
2017-07-14 15:08:09 +0200

    Remove logstash logger and clean up code

--- index.js
+++ index.js
@@ -3,14 +3,11 @@
 'use strict';
 
 var through = require('through2');
-var minimist = require('minimist');
 var allContainers = require('docker-allcontainers');
 var statsFactory = require('docker-stats');
 var logFactory = require('docker-loghose');
 var eventsFactory = require('./docker-event-log');
 var winston = require('winston');
-var winstonLogstash = require('winston-logstash');
-var isJSON = require('is-json');
 
 function start() {
   var events = allContainers({});
@@ -24,11 +21,6 @@ function start() {
         level: 'debug',
         formatter: function(options) { return options.message; }
       }),
-      new (winston.transports.Logstash)({
-        port: process.env.LOGSTASH_PORT,
-        host: process.env.LOGSTASH_HOST,
-        formatter: function(options) { return options.message; }
-      })
     ]
   })
 
